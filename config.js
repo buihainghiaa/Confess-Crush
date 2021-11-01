a7d150c341e72e42037e7e8d100d9947c57eeba4
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-11-01 20:00:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 const CONFIG = {
-	    titleWeb: "webgiday",
-	    introTitle: 'Chào các anh em b22',
-	       introDesc: `Trái đất vốn lạ thường`,
-	    btnIntro: 'alo',
-	    title: 'Mấy thằng b22 chơi gay không',
-	   desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-	    btnYes: 'CHơi luôn đê anh ơi',
-	    btnNo: 'THôi đéo',
-	    question: ' Lạc vào tim anh lẻ loi
-	    Đằng sau chữ yêu đây là thương
-	
-	',
-	    btnReply: 'hihi ',
-	    reply: 'chuẩn bị dầu ăn thôi hihi <33333333',
-	    mess: 'hẹn ae ngày 0 xa',
-	}
-
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
