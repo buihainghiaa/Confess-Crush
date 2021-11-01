db0aa9f74ab005620bbdbfec7eb59390b12a9168
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-11-01 19:13:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "ohhhhh",
-    introTitle: 'fsdfdsf',
+    introTitle: 'fsdghghghgfhf',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
