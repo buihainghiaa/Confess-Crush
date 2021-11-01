7b3a4bb199f6e4d44fb9a69de3aad768c685d820
buihainghiaa <93524625+buihainghiaa@users.noreply.github.com>
2021-11-01 19:36:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     titleWeb: "webgiday",
     introTitle: 'Chào các anh em b22',
-   
+       introDesc: `Trái đất vốn lạ thường`,
     btnIntro: 'alo',
     title: 'Mấy thằng b22 chơi gay không',
-   
+   desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'CHơi luôn đê anh ơi',
     btnNo: 'THôi đéo',
     question: ' Lạc vào tim anh lẻ loi
